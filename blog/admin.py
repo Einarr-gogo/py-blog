43d0bba0e004114f3ba2ba93dece1b679f42a223
Oleksandr <aleksandrpaskoo22@gmail.com>
2026-03-13 13:32:37 +0100

    Solution

--- blog/admin.py
+++ blog/admin.py
@@ -11,18 +11,21 @@ admin.site.unregister(Group)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "is_staff")
+    list_display = ("username", "email", "is_staff",)
+    search_fields = ("email", "username",)
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("owner__username", "title")
-    search_fields = ("owner__username", "title")
+    list_display = ("owner__username", "title",)
+    search_fields = ("owner__username", "title",)
 
 
 @admin.register(Commentary)
 class CommentaryAdmin(admin.ModelAdmin):
     list_display = (
+        "user__username",
         "post",
         "content",
     )
+    search_fields = ("user__username",)
