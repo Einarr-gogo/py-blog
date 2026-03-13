22a04d1316f2a6d0eb0329c215317aa6e381ea1b
Oleksandr <aleksandrpaskoo22@gmail.com>
2026-03-13 13:11:57 +0100

    Solution

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,8 @@ class User(AbstractUser):
 
 class Post(models.Model):
     owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name="posts"
     )
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=1000)
@@ -21,8 +22,11 @@ class Post(models.Model):
 
 class Commentary(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name="comments"
     )
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
     created_time = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=1000)
